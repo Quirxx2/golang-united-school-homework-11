1acac0c0eebe9f3abb496019371dd94f1599bf6d
cole jo <colejo@coles-MacBook-Pro.local>
2022-07-02 21:17:08 +0300

    First

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -8,11 +9,48 @@ type user struct {
 	ID int64
 }
 
-func getOne(id int64) user {
-	time.Sleep(time.Millisecond * 100)
-	return user{ID: id}
+func getOne(u chan user, task <-chan int64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		id, ok := <-task
+		if !ok {
+			return
+		}
+		time.Sleep(time.Millisecond * 100)
+		u <- user{ID: id}
+	}
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+
+	users := make(chan user, n)
+	var wg sync.WaitGroup
+	wg.Add(int(pool))
+
+	tasks := make(chan int64, pool)
+
+	var i int64
+	for i = 1; i <= pool; i++ {
+		go getOne(users, tasks, &wg)
+	}
+
+	var j int64
+	for j = 0; j < n; j++ {
+		tasks <- j
+	}
+
+	close(tasks)
+
+	wg.Wait()
+
+	var y int64
+	var r []user
+	for y = 1; y <= n; y++ {
+		x := <-users
+		r = append(r, x)
+	}
+
+	close(users)
+
+	return r
 }
